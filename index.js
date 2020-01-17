a0fa6a507ca711403a824684d94206c3002059b5
Gabby Jones <jones.gm90@gmail.com>
2020-01-17 17:07:49 -0500

    -m"command line rcving info "

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
+const puppeteer = require("puppeteer");
+const handlebars = require("handlebars");
 
 inquirer
-  .prompt({
-    message: "Enter your GitHub username, What is your favorite color?",
-    name: "username",
-    favoriteColor: "favoriteColor"
-  })
-  .then(function({ username, favoriteColor }) {
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+    .prompt([
+        {
+            message: "Enter your GitHub username",
+            name: "username",
+        },
+
+        {
+            message: "What is your favorite color?",
+            name: "favoriteColor",
+
+        }
+    ])
+    .then(function ({ username, favoriteColor }) {
+        const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
 
 
 
-  axios.get(queryUrl).then(function(res) {
-    console.log(res.data);
-  });
-  });
+        axios.get(queryUrl).then(function (res) {
+            console.log(res.data);
+        });
+    });
