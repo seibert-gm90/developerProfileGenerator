db693e4b757325d549166aa04a80da45f04c38c2
Gabby Jones <jones.gm90@gmail.com>
2020-01-18 21:09:58 -0500

    creating profile info object

--- index.js
+++ index.js
@@ -18,11 +18,33 @@ inquirer
         }
     ])
     .then(function ({ username, favoriteColor }) {
-        const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100/`;
-
-
+        const queryUrl = `https://api.github.com/users/${username}`;
 
         axios.get(queryUrl).then(function (res) {
-            console.log(res.data);
+            // console.log(res);
+            // console.log(Object.keys(res));     
+            console.log(res.data);       
+
+
+            let profile = {
+                followers: res.data.followers,
+                following: res.data.following,
+                location: res.data.location,
+                blog: res.data.blog,
+                userProfile: res.data.html_url,
+                profileImage: res.data.avatar_url,
+                userName: res.data.login,
+                stars: res.data.starred_url,
+                bio: res.data.bio,
+                publicRepos: res.data.public_repos
+            }
+            makeHTML(profile);
         });
+
     });
+
+function makeHTML(profile) {
+    
+    console.log(profile)
+}
+
