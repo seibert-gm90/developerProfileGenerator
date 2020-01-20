5b6c5d1d5eced75a0b0ea7a0581ca97fdc495d90
Gabby Jones <jones.gm90@gmail.com>
2020-01-20 18:07:41 -0500

    HTML to PDF working

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const fs = require("fs");
+const util = require("util");
 const axios = require("axios");
 const inquirer = require("inquirer");
 const puppeteer = require("puppeteer");
 const handlebars = require("handlebars");
 
-inquirer
-    .prompt([
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+function userPrompt(){
+    return inquirer.prompt([
         {
             message: "Enter your GitHub username",
             name: "username",
@@ -13,38 +17,140 @@ inquirer
 
         {
             message: "What is your favorite color?",
-            name: "favoriteColor",
+            name: "color",
 
         }
     ])
-    .then(function ({ username, favoriteColor }) {
-        const queryUrl = `https://api.github.com/users/${username}`;
-
-        axios.get(queryUrl).then(function (res) {
-            // console.log(res);
-            // console.log(Object.keys(res));     
-            console.log(res.data);       
-
-
-            let profile = {
-                followers: res.data.followers,
-                following: res.data.following,
-                location: res.data.location,
-                blog: res.data.blog,
-                userProfile: res.data.html_url,
-                profileImage: res.data.avatar_url,
-                userName: res.data.login,
-                stars: res.data.starred_url,
-                bio: res.data.bio,
-                publicRepos: res.data.public_repos
-            }
-            makeHTML(profile);
-        });
-
-    });
-
-function makeHTML(profile) {
-    
-    console.log(profile)
 }
 
+
+
+async function generateProfile(){
+    try {
+        const userInput = await userPrompt();
+
+        const ghRes = await axios.get(
+            `https://api.github.com/users/${userInput.username}`
+            );
+
+            // console.log(queryUrl)
+        
+
+        const ghstarsqueryURL = await axios.get(
+            `https://api.github.com/users/${userInput.username}/starred`
+            );
+            
+            console.log(ghstarsqueryURL)
+
+        const html = makeHTML(ghRes, userInput, ghstarsqueryURL);
+
+        
+    await writeFileAsync(`profile_${userInput.username}.html`, html);
+    console.log(`Successfully wrote profile_${userInput.username}.html`);
+
+    // Creation of .pdf file
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.setContent(html);
+    await page.emulateMediaFeatures("screen");
+    await page.pdf({
+      path: `profile_${userInput.username}.pdf`,
+      format: "A4",
+      printBackground: true
+    });
+
+    console.log(`Successfully wrote profile_${userInput.username}.pdf`);
+    await browser.close();
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+}
+    generateProfile()
+
+function makeHTML(ghRes, userInput, ghstarsqueryURL) {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Deleloper Profile Generator</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="index.js"></script>
+    
+    </head>
+    
+    <body>
+        <header>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="card dark">
+                            <img src="${ghRes.data.avatar_url}" height="175" />
+                            <h1>${ghRes.data.name}</h1>
+                            <p class="header-links d-flex justify-content-center flex-wrap">
+                                <a href="https://www.google.com/maps/place/${ghRes.data.location}">
+                                    ${ghRes.data.location}
+                                </a>
+                                <a href="${ghRes.data.userProfile}">GitHub Profile</a>
+                                <a href="${ghRes.data.blog}">Blog</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+        </header>
+        <div class="row">
+            <div class="col">
+                <p>${ghRes.data.bio}</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="card">
+                    <div class="card-body " id="public-repos">
+                        <h5>Repos</h5>
+                        <p>${ghRes.data.public_repos}Numbers</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="card">
+                    <div class="card-body " id="followers">
+                        <h5>Followers</h5>
+                        <p>${ghRes.data.followers}Number</p>
+                    </div>
+                </div>
+            </div>
+    
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-body " id="gh-stars">
+                            <h5>GitHub</h5>
+                            <p>${ghstarsqueryURL.data.length}StarsNumbers</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-body " id="following">
+                            <h5>Following</h5>
+                            <p>${ghRes.data.following}Number</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+    
+    </body>
+    
+    </html>`
+}
+        
+                                                    
+                                                                        
